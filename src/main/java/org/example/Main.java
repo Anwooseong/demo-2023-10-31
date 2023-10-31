423fef4094d53e908a447995bea2b9e2b767cd77
Anwooseong <aws5624@naver.com>
2023-10-31 09:05:57 +0900

    커밋1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("커밋1");
+        System.out.println("커밋2");
     }
 }
